6dcce3a43fb7cb0c698e14b15957f31c446d43bf
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-12-02 15:40:01 +0300

    completing-react-parts-remember-events

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -8,11 +8,13 @@ export class Postform extends Component {
       body: ""
     };
 
-    //so that you may bind to the state when event change
+    //Have a direct effect in the class change of state.
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  //i have alreadt set the state by the on change
+  //therefore only submit remains!
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
@@ -31,16 +33,18 @@ export class Postform extends Component {
         "content-type": "application/json"
       },
       body: JSON.stringify(post)
-    }).then(res => res.json());
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
   }
-  onSubmit(e) {}
+
   render() {
     return (
       <div>
         <h1>Add Post</h1>
         <form onSubmit={this.onSubmit}>
           <div>
-            <label>Title:</label>
+            <label>Title: </label>
             <br />
             <input
               type='text'
@@ -51,17 +55,15 @@ export class Postform extends Component {
           </div>
           <br />
           <div>
-            <label>Body:</label>
+            <label>Body: </label>
             <br />
             <textarea
               name='body'
-              type='text'
               onChange={this.onChange}
               value={this.state.body}
             />
           </div>
           <br />
-
           <button type='submit'>Submit</button>
         </form>
       </div>
